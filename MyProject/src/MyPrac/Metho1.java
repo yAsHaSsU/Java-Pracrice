41fd43a4cd886c7169d51a222caaaf1ae5d3dd9d
yAsHaSsU <yashasureddy@gmail.com>
2024-03-21 19:16:43 +0530

    java practice

--- MyProject/src/MyPrac/Metho1.java
+++ MyProject/src/MyPrac/Metho1.java
@@ -1,9 +1,9 @@
 package MyPrac;
 
 public class Metho1 {
-    /*       static void multiplication(int n) {
+           static void multiplication(int n) {
         	   for(int i=1;i<=10000;i++) {
-        		   System.out.format("%d X %d = %d\n",n,i,n*i);           multiplucation
+        		   System.out.format("%d X %d = %d\n",n,i,n*i);           
         	   }
            }
 	public static void main(String[] args) {
@@ -11,19 +11,5 @@ public class Metho1 {
 multiplication(7);
 	}
 
-}*/
-	
-	
-	static void pattern(int n) {
-	   	for(int i =0; i<n ; i++) {
-			for(int j=0;j<i+1;j++) {
-				System.out.print("*");
-			}
-			System.out.println( " ");
-		}
-	}
-	public static void main(String[] args) {
-		pattern(4);
-	}
 }
 	
